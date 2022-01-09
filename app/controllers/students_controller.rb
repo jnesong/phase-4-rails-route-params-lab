6842bc428ea280b6aa001122037e600b053f6dc1
Jenny Chau <jnechau@gmail.com>
2022-01-08 22:21:18 -0500

    commit tree 0cb15d551df5a59ecd09f290f3c4ddb5c7482b07

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def sup
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
 end
